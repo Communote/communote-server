02e2b3039106012f225fa0582bcc9520c3ffb9f7
Ronny Winkler <ronny.winkler@communote.com>
2016-07-01 20:09:01 +0200

    [BM] made remaining build modules open-source

---
+++ build/ear/README.md
@@ -0,0 +1,1 @@
+Note: this is untested.
---
+++ build/installation-package-assembly/src/main/resources/tomcat/bin/windows-x86/setenv.sh
@@ -0,0 +1,1 @@
+JAVA_OPTS="${JAVA_OPTS} -Djava.awt.headless=true -XX:MaxPermSize=256m -Xms256m -Xmx512m -Dorg.apache.jasper.compiler.Parser.STRICT_QUOTE_ESCAPING=false"
