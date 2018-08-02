b6e1a70020d14e3e33d54071f34a61af700f313c
rwi <rikudou@web.de>
2018-08-02 20:58:34 +0200

    [BM] #34: update Tomcat to version 8.5.32
    - update packaging so that Tomcat 8.5.32 is bundled into the installer
    packages
    - fixed markdown syntax errors in README and improved documentation
    - removed outdated JAVA_OPTS
    - removed outdated and invalid parameters from server.xml

--- build/installation-package-assembly/src/main/resources/tomcat/bin/setenv.sh
+++ build/installation-package-assembly/src/main/resources/tomcat/bin/setenv.sh
@@ -1,1 +1,1 @@
-JAVA_OPTS="${JAVA_OPTS} -server -Djava.awt.headless=true -XX:MaxPermSize=256m -Xms256m -Xmx512m -Dorg.apache.jasper.compiler.Parser.STRICT_QUOTE_ESCAPING=false"
+JAVA_OPTS="${JAVA_OPTS} -server -Djava.awt.headless=true -Xms256m -Xmx512m -Dorg.apache.jasper.compiler.Parser.STRICT_QUOTE_ESCAPING=false"
